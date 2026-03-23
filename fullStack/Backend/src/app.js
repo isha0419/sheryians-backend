93b98d1827a081554919a0d82e26737cf0fde091
isha0419 <isha01092004@gmail.com>
2026-03-23 07:52:12 +0530

    first commit

---
+++ fullStack/Backend/src/app.js
@@ -0,0 +1,59 @@
+const express = require("express")
+const noteModel = require("./models/notes.models")
+const cors = require("cors")
+const path = require("path")
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+app.use(express.static(path.join(__dirname, "..", "public")))
+
+// POST
+app.post("/api/notes", async (req,res) =>{
+    const {title , description} = req.body
+    const note = await noteModel.create({
+        title , description
+    })
+    res.status(201).json({
+        message : "Note created succesfully",
+        note
+    })
+})
+
+// GET
+app.get("/api/notes" , async (req,res)=>{
+    const notes = await noteModel.find()
+
+    res.status(200).json({
+        message: "Notes fetched successfully",
+        notes
+    })
+})
+
+// DELETE
+app.delete("/api/notes/:id",async(req,res)=>{
+    const id= req.params.id
+    await noteModel.findByIdAndDelete(id)
+
+    res.status(200).json({
+        message : "Note Deleted successfully"
+    })
+})
+
+// UPDATE
+app.patch("/api/notes/:id", async (req,res)=>{
+    const id = req.params.id
+    const {description} = req.body
+
+    await noteModel.findByIdAndUpdate(id, {description})
+
+    res.status(200).json({
+        message : "Note updated successfully."
+    })
+})
+
+app.use('*name',(req,res)=>{
+    res.sendFile(path.join(__dirname,"..","/public/index.html"))
+})
+module.exports = app
---
+++ fullStack/Frontend/src/App.jsx
@@ -0,0 +1,70 @@
+import { useEffect, useState } from 'react'
+import axios from "axios"
+
+
+function App() {
+    const [notes, setnotes] = useState([])
+
+    console.log("hello integration")
+
+    function fetchNotes(){
+      axios.get("http://localhost:3000/api/notes")
+      .then((res)=>{
+        setnotes(res.data.notes)
+      })
+    }
+
+    useEffect(()=>{
+        fetchNotes()
+    },[])
+
+    function handleSubmit(e){
+      e.preventDefault()
+      const {title,description} = e.target.elements
+
+      axios.post("http://localhost:3000/api/notes",{
+        title: title.value,
+        description : description.value
+      })
+      .then(res=>{
+        console.log(res.data)
+        fetchNotes()
+      })
+    }
+
+    function handleDelete(noteId){
+
+      axios.delete("http://localhost:3000/api/notes/"+noteId)
+      .then(res=>{
+        console.log(res.data)
+        fetchNotes()
+      })
+
+    }
+
+    return (
+      <>
+        
+        <form className='note-create-form' onSubmit={handleSubmit}>
+          <input name="title" type="text" placeholder='Enter title'/>
+          <input name="description" type="text" placeholder='Enter description'/>
+          <button >Create note </button>
+        </form>
+         
+        <div className="notes">
+          {
+            notes.map(note =>{
+              return <div className="note">
+                        <h1>{note.title}</h1>
+                        <p>{note.description}</p>
+                        <button onClick={()=>{handleDelete(note._id)}}>delete</button>
+                     </div>
+            })
+          }
+          
+        </div>
+      </>
+    )
+}
+
+export default App
